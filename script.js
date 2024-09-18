ca769bd945f4d4fdfc75856884dcfcce87355bc3
costbuildup <costbuildup@gmail.com>
2024-09-18 18:59:37 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+document.getElementById("budgetForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const initialBudget = parseFloat(
+    document.getElementById("initialBudget").value
+  );
+  const years = parseInt(document.getElementById("years").value);
+  const inflationRate =
+    parseFloat(document.getElementById("inflationRate").value) / 100;
+
+  let finalBudget = initialBudget;
+  let totalBudget = 0;
+
+  for (let i = 0; i < years; i++) {
+    finalBudget *= 1 + inflationRate;
+    totalBudget += finalBudget;
+  }
+
+  const totalBudgetFormatted = totalBudget.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD"
+  });
+
+  document.getElementById(
+    "result"
+  ).innerHTML = `Total budget after ${years} years: ${totalBudgetFormatted}`;
+});
